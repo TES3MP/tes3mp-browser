9edbf3b7e4268a265f582760c8a2b64443fda717
Koncord <koncord@rwa.su>
2018-09-11 00:40:30 +0800

    Fix building on Windows

--- apps/browser/Version.hpp
+++ apps/browser/Version.hpp
@@ -5,12 +5,13 @@
 #pragma once
 
 #include <string>
+#include <cstdint>
 
 #define MAJOR_VERSION 1
 #define MINOR_VERSION 0
 
-unsigned long constexpr make_version(unsigned long major, unsigned long minor) { return (major << 32) + minor; }
-unsigned long constexpr version() { return make_version(MAJOR_VERSION, MINOR_VERSION); }
+uint64_t constexpr make_version(uint64_t major, uint64_t minor) { return (major << 32) + minor; }
+uint64_t constexpr version() { return make_version(MAJOR_VERSION, MINOR_VERSION); }
 inline std::string strVersion() noexcept { return std::to_string(MAJOR_VERSION) + "." + std::to_string(MINOR_VERSION); }
 
 #define FIX_UNTIL(_version, message) static_assert(version() < _version, message)
